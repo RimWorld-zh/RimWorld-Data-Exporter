42da61240cdd0ecf3f0ad23592231734e205b6ce
DUDULUU-R7\duduluu <duduluu@outlook.com>
2017-11-26 20:39:23 +0800

    Add damage armor category model

---
+++ Source/Model/DamageArmorCategory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using Verse;
+using RimWorld;
+using RimWorldDataExporter.Helper;
+
+namespace RimWorldDataExporter.Model {
+    class DataDamageArmorCategory : EData {
+        public override string Category => "DamageArmorCategory";
+        public override Type DefType => typeof(DamageArmorCategoryDef);
+
+        public StatDef multStat;
+        public StatDef deflectionStat;
+    }
+}
