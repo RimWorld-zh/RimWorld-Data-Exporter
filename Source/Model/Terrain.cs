83f6a5651a952e7afc1d03d83385929a2ce7d8ba
duduluu <duduluu@outlook.com>
2017-12-24 02:58:01 +0800

    Add model: Terrain

---
+++ Source/Model/Terrain.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using Verse;
+using RimWorld;
+using RimWorldDataExporter.Helper;
+
+namespace RimWorldDataExporter.Model {
+    class DataTerrain : DataBuildable {
+        public static readonly List<StatDef> allStatDefs = new List<StatDef> {
+            StatDefOf.Beauty,
+            StatDefOf.Cleanliness,
+            StatDefOf.Flammability,
+            StatDefOf.WorkToBuild,
+            StatDefOf.MarketValue,
+        };
+
+        public override string Category => "Terrain";
+        public override Type DefType => typeof(TerrainDef);
+        
+        public Traversability passability;
+        public int pathCost;
+        public bool pathCostIgnoreRepeat = true;
+        public float fertility = -1f;
+        public TerrainAffordance terrainAffordanceNeeded = TerrainAffordance.Light;
+        public List<ResearchProjectDef> researchPrerequisites;
+        public int constructionSkillPrerequisite;
+        public float resourcesFractionWhenDeconstructed = 0.75f;
+        public DesignationCategoryDef designationCategory;
+        public KeyBindingDef designationHotKey;
+        public TechLevel minTechLevelToBuild;
+        public TechLevel maxTechLevelToBuild;
+
+        [NoCrawl]
+        public List<FloatDefValue> stats = new List<FloatDefValue>();
+
+        public override void Crawl(Def baseDef) {
+            base.Crawl(baseDef);
+
+            var def = baseDef as TerrainDef;
+
+            foreach (StatDef statDef in allStatDefs) {
+                stats.Add(new FloatDefValue(statDef, def.GetStatValueAbstract(statDef)));
+            }
+        }
+    }
+    
+    class LangTerrain : ELang {
+        public override string Category => "Terrain";
+        public override Type DefType => typeof(TerrainDef);
+    }
+}
