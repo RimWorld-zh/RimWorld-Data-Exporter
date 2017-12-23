daa3bedb15f8cc5bcd9bfbc762c3317eb69645c1
duduluu <duduluu@outlook.com>
2017-12-24 02:57:13 +0800

    Update BasicModel.cs

--- Source/Model/BasicModel.cs
+++ Source/Model/BasicModel.cs
@@ -2,15 +2,32 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Verse;
+using RimWorld;
 
 namespace RimWorldDataExporter.Model {
-    
-    class FloatDefValue: Helper.Serialization.ITypeable {
+
+    class FloatDefValue : Helper.Serialization.ITypeable {
         string defName;
         float value;
+
         public FloatDefValue(string defName, float value) {
             this.defName = defName;
             this.value = value;
         }
+
+        public FloatDefValue(Def def, float value) : this(def.defName, value) { }
+    }
+
+    class IntDefValue : Helper.Serialization.ITypeable {
+        string defName;
+        int value;
+
+        public IntDefValue(string defName, int value) {
+            this.defName = defName;
+            this.value = value;
+        }
+
+        public IntDefValue(Def def, int value) : this(def.defName, value) { }
     }
 }
