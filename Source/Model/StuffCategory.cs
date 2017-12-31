26b7843fb9d54fa547bcbcd103ba32cc929381be
duduluu <duduluu@outlook.com>
2018-01-01 03:00:57 +0800

    Update Model StuffCategory

--- Source/Model/StuffCategory.cs
+++ Source/Model/StuffCategory.cs
@@ -11,6 +11,20 @@ namespace RimWorldDataExporter.Model {
     class DataStuffCategory : EData {
         public override string Category => "StuffCategory";
         public override Type DefType => typeof(StuffCategoryDef);
+
+        [NoCrawl]
+        public List<ThingDef> stuffs;
+
+        public override void Crawl(Def baseDef) {
+            base.Crawl(baseDef);
+
+            var def = baseDef as StuffCategoryDef;
+
+            this.stuffs = DefDatabase<ThingDef>
+                            .AllDefs
+                            .Where(t => t.IsStuff && t.stuffProps.categories.Contains(def))
+                            .ToList();
+        }
     }
 
     class LangStuffCategory : ELang {
