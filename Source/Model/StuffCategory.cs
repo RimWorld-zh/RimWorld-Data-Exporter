268ba71b927a463560962ceef6f37ae92ed63ade
duduluu <duduluu@outlook.com>
2018-01-01 02:11:54 +0800

    Add Model StuffCategory

---
+++ Source/Model/StuffCategory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using Verse;
+using RimWorld;
+using RimWorldDataExporter.Helper;
+
+namespace RimWorldDataExporter.Model {
+    class DataStuffCategory : EData {
+        public override string Category => "StuffCategory";
+        public override Type DefType => typeof(StuffCategoryDef);
+    }
+
+    class LangStuffCategory : ELang {
+        public override string Category => "StuffCategory";
+        public override Type DefType => typeof(StuffCategoryDef);
+    }
+}
