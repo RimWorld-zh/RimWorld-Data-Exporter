d4f036ed8912dda331589336744a466f963b8be3
duduluu <duduluu@outlook.com>
2018-01-07 22:49:28 +0800

    Update Model Skill: add aggregation

--- Source/Model/Skill.cs
+++ Source/Model/Skill.cs
@@ -11,7 +11,7 @@ namespace RimWorldDataExporter.Model {
     class DataSkill : EData {
         public override string Category => "Skill";
         public override Type DefType => typeof(SkillDef);
-        
+
         [NoCrawl]
         public List<WorkTags> workTags;
 
@@ -20,7 +20,78 @@ namespace RimWorldDataExporter.Model {
 
             var def = baseDef as SkillDef;
 
-            this.workTags = Helper.Serialization.DataExtension.GetUniqueFlags(def.disablingWorkTags).ToList();
+            this.workTags =
+                Helper
+                    .Serialization
+                    .DataExtension
+                    .GetUniqueFlags(def.disablingWorkTags)
+                    .ToList();
+        }
+
+        // Aggregation
+
+        class AggrSkill : EAggr {
+            public readonly int intervalTicks = SkillRecord.IntervalTicks;
+
+            public readonly int minLevel = SkillRecord.MinLevel;
+            public readonly int maxLevel = SkillRecord.MaxLevel;
+
+            public readonly int maxFullRateXpPerDay = SkillRecord.MaxFullRateXpPerDay;
+
+            public readonly int masterSkillThreshold = SkillRecord.MasterSkillThreshold;
+
+            public readonly float saturatedLearningFactor = SkillRecord.SaturatedLearningFactor;
+
+            public readonly float learnFactorPassionNone = SkillRecord.LearnFactorPassionNone;
+            public readonly float learnFactorPassionMinor = SkillRecord.LearnFactorPassionMinor;
+            public readonly float learnFactorPassionMajor = SkillRecord.LearnFactorPassionMajor;
+
+            public readonly double joyGainFactorPassionNone = 0.0;
+            public readonly double joyGainFactorPassionMinor = 1.9999999494757503E-05;
+            public readonly double joyGainFactorPassionMajor = 3.9999998989515007E-05;
+
+            public readonly List<float> xpRequirements = new List<float>();
+            public readonly List<float> xpLosses = new List<float>();
+
+            public static float XpLossFrom(int level) {
+                switch (level) {
+                case 10:
+                    return -0.1f;
+                case 11:
+                    return -0.2f;
+                case 12:
+                    return -0.4f;
+                case 13:
+                    return -0.6f;
+                case 14:
+                    return -1f;
+                case 15:
+                    return -1.8f;
+                case 16:
+                    return -2.8f;
+                case 17:
+                    return -4f;
+                case 18:
+                    return -6f;
+                case 19:
+                    return -8f;
+                case 20:
+                    return -12f;
+                default:
+                    return 0;
+                }
+            }
+
+            public AggrSkill() {
+                for (int level = 0; level <= 20; level++) {
+                    xpRequirements.Add(SkillRecord.XpRequiredToLevelUpFrom(level));
+                    xpLosses.Add(AggrSkill.XpLossFrom(level));
+                }
+            }
+        }
+
+        public override EAggr GetAggregation() {
+            return new AggrSkill();
         }
     }
     class LangSkill : ELang {
