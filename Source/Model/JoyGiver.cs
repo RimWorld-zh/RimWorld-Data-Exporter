7a714f589f07344a6d9d2c6ffdf8415b5e0a4598
duduluu <duduluu@outlook.com>
2018-01-07 20:09:45 +0800

    Add Model JoyGiver

---
+++ Source/Model/JoyGiver.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using Verse;
+using RimWorld;
+using RimWorldDataExporter.Helper;
+
+namespace RimWorldDataExporter.Model {
+    class DataJoyGiver : EData {
+        public override string Category => "JoyGiver";
+        public override Type DefType => typeof(JoyGiverDef);
+
+        public float baseChance;
+        public List<ThingDef> thingDefs;
+        public JobDef jobDef;
+        public JoyKindDef joyKind;
+        public bool desireSit = true;
+        public float pctPawnsEverDo = 1f;
+        public bool unroofedOnly;
+        public List<PawnCapacityDef> requiredCapacities = new List<PawnCapacityDef>();
+        public bool canDoWhileInBed;
+    }
+}
