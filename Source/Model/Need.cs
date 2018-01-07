ca0256b69104a460a369e791b7ce68ecf406627e
duduluu <duduluu@outlook.com>
2018-01-07 23:08:18 +0800

    Add Model Need

---
+++ Source/Model/Need.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using Verse;
+using RimWorld;
+using RimWorldDataExporter.Helper;
+
+namespace RimWorldDataExporter.Model {
+    class DataNeed : EData {
+        public override string Category => "Need";
+        public override Type DefType => typeof(NeedDef);
+
+        public Intelligence minIntelligence;
+        public bool colonistAndPrisonersOnly;
+        public bool colonistsOnly;
+        public bool onlyIfCausedByHediff;
+        public bool neverOnPrisoner;
+        public bool showOnNeedList = true;
+        public float baseLevel = 0.5f;
+        public bool major;
+        public int listPriority;
+        public string tutorHighlightTag;
+        public bool showForCaravanMembers;
+        public bool scaleBar;
+        public float fallPerDay = 0.5f;
+        public float seekerRisePerHour;
+        public float seekerFallPerHour;
+        public bool freezeWhileSleeping;
+    }
+}
