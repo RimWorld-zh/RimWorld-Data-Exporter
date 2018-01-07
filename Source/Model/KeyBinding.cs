cadae9c1c86fc1cc5f8a25457c9ed6f59ab5b11a
duduluu <duduluu@outlook.com>
2018-01-08 01:16:55 +0800

    Add Model KeyBinding

---
+++ Source/Model/KeyBinding.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using Verse;
+using RimWorld;
+using RimWorldDataExporter.Helper;
+
+namespace RimWorldDataExporter.Model {
+    class DataKeyBinding : EData {
+        public override string Category => "KeyBinding";
+        public override Type DefType => typeof(KeyBindingDef);
+
+        public KeyBindingCategoryDef category;
+        public KeyCode defaultKeyCodeA;
+        public KeyCode defaultKeyCodeB;
+    }
+
+    class LangKeyBinding : ELang {
+        public override string Category => "KeyBinding";
+        public override Type DefType => typeof(KeyBindingDef);
+    }
+}
---
+++ Source/Model/KeyBindingCategory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using Verse;
+using RimWorld;
+using RimWorldDataExporter.Helper;
+
+namespace RimWorldDataExporter.Model {
+    class DataKeyBindingCategory : EData {
+        public override string Category => "KeyBindingCategory";
+        public override Type DefType => typeof(KeyBindingCategoryDef);
+    }
+
+    class LangKeyBindingCategory : ELang {
+        public override string Category => "KeyBindingCategory";
+        public override Type DefType => typeof(KeyBindingCategoryDef);
+    }
+}
